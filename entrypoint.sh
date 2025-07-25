8df409b3f59b2e887edce9cc2cfb3976982d95bc
Louis Garoche <louis@garoche.me>
2025-07-25 11:24:25 +0200

    fix: websocket config

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ fi
 wsopts=""
 if [ "$ENABLE_WS" = "true" ]; then
   echo "WebSocket interface enabled."
-  wsopts="--ws true --ws-port ${WS_PORT:-6061} --ws-host ${WS_HOST:-0.0.0.0}"
+  wsopts="--ws --ws-port ${WS_PORT:-6061} --ws-host ${WS_HOST:-0.0.0.0}"
 fi
 
 juno \
